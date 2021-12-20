6dedf9d3e7f0fc1f04791220357f850642ec47e1
dwaGithub <78429825+dwaGithub@users.noreply.github.com>
2021-12-20 13:15:47 +0100

    Initial commit

---
+++ src/main/java/edu/kea/municipal_election_backend/MunicipalElectionBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.kea.municipal_election_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipalElectionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunicipalElectionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/kea/municipal_election_backend/MunicipalElectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kea.municipal_election_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalElectionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
